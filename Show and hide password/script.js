6b6b1559a14d352783c2da3a6ca643728bd8ff0d
Baraa Mahdy <mahdyb980@gmail.com>
2024-05-30 11:09:46 +0300

    v3

--- Show and hide password/script.js
+++ Show and hide password/script.js
@@ -4,8 +4,9 @@ let eyeicon = document.getElementById("eyeicon");
 eyeicon.onclick = function(){
     if(password.type == "password"){
         password.type = "text";
+        eyeicon.src = "/Show and hide password/images/show.png";
     } else{
         password.type = "password";
-
+        eyeicon.src = "/Show and hide password/images/hide.png";
     }
 }
